63bc3b355e88e67b1f4fa5dc86d4646b2d9669f3
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2024-10-23 08:53:42 +0530

    Wee-2-js todos

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -9,5 +9,14 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const timeStart = new Date().getSeconds();
+    var sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const timeEnd = new Date().getSeconds();
+    return timeEnd - timeStart;
 }
+
+console.log(calculateTime(1000000000));
